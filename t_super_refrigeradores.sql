36da18a3b638214e292aa67285686113ccd2e14b
EdAlpharo <72086965+EdAlpharo@users.noreply.github.com>
2021-07-19 21:08:02 -0500

    Add files via upload

--- t_super_refrigeradores.sql
+++ t_super_refrigeradores.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 04-07-2021 a las 02:48:56
+-- Tiempo de generación: 19-07-2021 a las 19:09:55
 -- Versión del servidor: 5.5.8
 -- Versión de PHP: 5.3.5
 
@@ -39,6 +39,51 @@ CREATE TABLE IF NOT EXISTS `admin` (
 
 INSERT INTO `admin` (`RFC`, `nombre`, `password`, `NSS`) VALUES
 ('EIMJ010202509', 'Jose', 'Codiniqw123', '23425234234234');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bitacora`
+--
+
+CREATE TABLE IF NOT EXISTS `bitacora` (
+  `idBitacora` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `accion` varchar(255) NOT NULL,
+  `observacion` varchar(255) NOT NULL,
+  `idIncidente` int(11) DEFAULT NULL,
+  `idViaje` int(11) NOT NULL,
+  PRIMARY KEY (`idBitacora`),
+  UNIQUE KEY `idViaje` (`idViaje`),
+  UNIQUE KEY `idIncidente` (`idIncidente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `bitacora`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `incidente`
+--
+
+CREATE TABLE IF NOT EXISTS `incidente` (
+  `idIncidente` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `ubicacion` varchar(255) NOT NULL,
+  `observacion` varchar(255) NOT NULL,
+  PRIMARY KEY (`idIncidente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `incidente`
+--
+
 
 -- --------------------------------------------------------
 
@@ -94,3 +139,46 @@ CREATE TABLE IF NOT EXISTS `unidad` (
 INSERT INTO `unidad` (`marca`, `modelo`, `numero_serie`, `ejes`, `largo`, `alto`, `ancho`, `Carga_maxima`, `Refrigerado`, `foto`, `placas`) VALUES
 ('Dongfeng', 'DFL1140B10-KX2V', '2352346232', 4, '9 m.', '2.6 m.', '3.92 m.', '15 TON', 1, '../Unidades/camionprueba.jpg', 'Edua4235'),
 ('Dongfeng', 'DFL1140B10-KX2V', '2352346234', 4, '9 m.', '2.6 m.', '3.92 m.', '15 TON', 1, '../Unidades/camionprueba.jpg', 'edd23157');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viaje`
+--
+
+CREATE TABLE IF NOT EXISTS `viaje` (
+  `idViaje` int(11) NOT NULL AUTO_INCREMENT,
+  `empresaEnvia` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `empresaRecibe` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `tipoBien` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `fechaEnvio` date NOT NULL,
+  `fechaEntrega` date NOT NULL,
+  `idUnidad` varchar(60) CHARACTER SET utf8 NOT NULL,
+  `RFC` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`idViaje`),
+  UNIQUE KEY `idUnidad` (`idUnidad`),
+  UNIQUE KEY `idOperador` (`RFC`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `viaje`
+--
+
+
+--
+-- Filtros para las tablas descargadas (dump)
+--
+
+--
+-- Filtros para la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
+  ADD CONSTRAINT `bitacora_ibfk_1` FOREIGN KEY (`idIncidente`) REFERENCES `incidente` (`idIncidente`),
+  ADD CONSTRAINT `bitacora_ibfk_2` FOREIGN KEY (`idViaje`) REFERENCES `viaje` (`idViaje`);
+
+--
+-- Filtros para la tabla `viaje`
+--
+ALTER TABLE `viaje`
+  ADD CONSTRAINT `viaje_ibfk_2` FOREIGN KEY (`idUnidad`) REFERENCES `unidad` (`numero_serie`),
+  ADD CONSTRAINT `viaje_ibfk_1` FOREIGN KEY (`RFC`) REFERENCES `operador` (`RFC`);
