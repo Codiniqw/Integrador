30d1be30a7490fd766cdb7650aec44c7464ec115
Froydev <73206961+Froydev@users.noreply.github.com>
2021-07-31 20:55:59 -0500

    Inicio de Secion v1

--- registroUnidad.php
+++ registroUnidad.php
@@ -6,6 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro Unidades</title>
+    <?php
+  session_start();
+  $Usession=$_SESSION['user'];
+  if ($Usession == null || $Usession=='') {
+    echo '<script> alert("inicia session  para poder comenzar"); window.location="index.php"; </script>';
+  }
+   ?>
     <link rel="icon" href="img/delivery.png" type="image/png" size="64x64">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous" />
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
