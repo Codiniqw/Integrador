26f0f4b0fe645ac29fe5f51782d51e672e11b42b
Froydev <73206961+Froydev@users.noreply.github.com>
2021-07-31 22:06:05 -0500

    Usuario

--- FuncionesBD.php
+++ FuncionesBD.php
@@ -75,4 +75,24 @@ function consultarviaje()
     }catch (Exception $e) {
     die('Exepcion capturada: '.$e-> getMessage());
 }
+// validar Operador
+function ValidateOPerator($user, $password)
+{
+    $conex = connect(); //Guarda la conexion con la base de datos
+    $query = "select password from operador where RFC = '$user' "; //consulta a relizar
+
+    try { // intenta realizar el processo
+        $validation = mysqli_query($conex, $query); //gaurda los datos obtenidos de la consulta
+        $numReg = mysqli_num_rows($validation); //Consulta el numero de registros obtenidos
+        $arrayBD = mysqli_fetch_array($validation); //Convierte los datos obtenidos en cadena de texto
+        mysqli_close($conex); //Termina la conexion con la BD
+
+        if (($numReg == 1) && ($password == $arrayBD['password'])  && (count_chars($password) >= 8))
+         { //if que valida que los datos sean iguales
+           session_start();
+           $_SESSION['user']= $user;
+            $status = 1;
+        } else {
+            $status = 0;
+        }
 }
