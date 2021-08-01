e9839594fde997673bbda79ac0d5a3c40ac6561d
codiniqw <019031480@upq.edu.mx>
2021-07-31 22:40:52 -0500

    correcion login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,4 @@
 function admin(){
-    var boton = document.getElementById("btnadmin");
-    boton.addEventListener("click", () => {
       document.getElementById("admin").style.display="block";
       document.getElementById("operador").style.display="none";
-    });
 }
