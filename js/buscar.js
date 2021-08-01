1d9b04964d1b75ca6fcb13c06e137131c5fd9689
codiniqw <019031480@upq.edu.mx>
2021-07-31 21:45:02 -0500

    metodos Buscar js

--- js/buscar.js
+++ js/buscar.js
@@ -1,3 +1,28 @@
 function buscarOperador(){
+    function buscarOperador(){
+        var boton = document.getElementById("Buscar");
     
+    boton.addEventListener("click", () => {
+    
+      var input = document.getElementById("buscador");
+      var valor = input.value;
+    
+      alert("El valor del campo es:"+ valor);
+      
+    });
+    
+    }
+    
+    function buscarUnidad(){
+        var boton = document.getElementById("BuscarU");
+    
+    boton.addEventListener("click", () => {
+    
+      var input = document.getElementById("buscador");
+      var valor = input.value;
+    
+      alert("El valor del campo es:"+ valor);
+      
+    });
+    }
 }
